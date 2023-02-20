edb4ed3a1a50c466ee157e7036e30b18c18855e6
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-02-20 01:43:50 +0000

    Train Sentiment Model Using NaiveBayes classification

--- train_model.py
+++ train_model.py
@@ -1,18 +1,19 @@
-from pyspark import SparkContext,SparkConf
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
+from pyspark.sql.functions import udf, col
 from pyspark.sql.types import ArrayType, StringType
+from pyspark.ml.feature import NGram
+from pyspark.ml.feature import StringIndexer
 import re
-from pyspark.ml.feature import StopWordsRemover, CountVectorizer, IDF, StringIndexer
 from pyspark.ml import Pipeline
-from pyspark.ml.classification import RandomForestClassifier,LogisticRegression
+from pyspark.ml.feature import StopWordsRemover, CountVectorizer, IDF
+from pyspark.ml.classification import NaiveBayes
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pathlib import Path
+
 SRC_DIR = Path(__file__).resolve().parent
 
-
 def clean_tweet(tweet):
-    #lowercase and remove hashtags, mentions, links, punctuations, etc.
     temp = tweet.lower()
     temp = re.sub("@[A-Za-z0-9_]+","", temp)
     temp = re.sub("#[A-Za-z0-9_]+","", temp)
@@ -25,25 +26,12 @@ def clean_tweet(tweet):
 
 # Create Spark Context and Spark Session
 conf = SparkConf().setAppName("EmotionPrediction")
-conf.set("spark.driver.memory", "4g")
-conf.set("spark.executor.memory", "4g")
+conf.set("spark.driver.memory", "8g")
+conf.set("spark.executor.memory", "8g")
 sc = SparkContext(conf=conf)
 spark = SparkSession.builder.appName("EmotionPrediction").getOrCreate()
 
 # Load data
-'''
-Emotion Dataset:
-Label - Emotion
-1     -   joy
-2     -   neutral
-3     -   trust
-4     -   anger
-5     -   fear
-6     -   surprise
-7     -   sadness
-8     -   anticipation
-9     -   disgust
-'''
 dataset_path = './Dataset/labelled_data.csv'
 df = (
     spark
@@ -52,25 +40,38 @@ df = (
     .load(dataset_path)
 )
 
+# Assuming 'df' is the dataframe
+print("Size of dataframe: ", df.count())
+
 # Clean and pre-process the tweet text
 pre_process = udf(lambda x: clean_tweet(x), ArrayType(StringType()))
 df = df.withColumn("processed_data", pre_process(df["Tweet Text"])).dropna()
 
 # Split the data into training and testing sets
-train, test = df.randomSplit([0.7, 0.3], seed = 13)
+train, test = df.randomSplit([0.8, 0.2], seed = 13)
 
-# Create ML pipeline
+# Define the preprocessing pipeline
 stop_word_remover = StopWordsRemover(inputCol="processed_data", outputCol="words")
-vectorizer = CountVectorizer(inputCol="words", outputCol="tf")
-idf = IDF(inputCol="tf", outputCol="features", minDocFreq=3)
-label_indexer = StringIndexer(inputCol = "Emotion Label", outputCol = "label")
-logistic_regression = LogisticRegression(maxIter=100)
+vectorizer = CountVectorizer(inputCol="words", outputCol="tf", vocabSize=10000, minDF=5)
+ngram = NGram(n=2, inputCol="words", outputCol="bigrams")
+idf = IDF(inputCol="tf", outputCol="features", minDocFreq=8)
+indexer = StringIndexer(inputCol="Emotion", outputCol="label")
+# Define the Naive Bayes Classifier
+nb = NaiveBayes(labelCol="label", featuresCol="features", smoothing=2)
 
-pipeline = Pipeline(stages=[stop_word_remover, vectorizer, idf, label_indexer, logistic_regression])
+# Define the pipeline
+pipeline = Pipeline(stages=[stop_word_remover,ngram, vectorizer, idf, indexer, nb])
 
-train.show(5)
-# Train the model
+# Train the model on training set
 model = pipeline.fit(train)
+
+# Get the StringIndexerModel object
+indexer_model = model.stages[4]
+
+# Show the mapping between emotion and label
+print("Mapping between Emotion and label:")
+for i, emotion in enumerate(indexer_model.labels):
+    print(f"{emotion}: {i}")
 
 # Predict on test set
 prediction = model.transform(test)
@@ -80,7 +81,7 @@ evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="p
 accuracy = evaluator.evaluate(prediction)
 
 print('\n***********************************************')
-print(f"Accuray of prediction: {accuracy}") 
+print(f"Accuracy score: {accuracy}")
 print('\n***********************************************')
 
 model_path = str(SRC_DIR.joinpath('models'))
