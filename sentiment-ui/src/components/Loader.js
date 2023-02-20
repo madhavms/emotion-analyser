1803775ce13bb38eb486a7e391bf30c41ef200b8
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-02-20 01:42:04 +0000

    Loader Component

---
+++ sentiment-ui/src/components/Loader.js
@@ -0,0 +1,8 @@
+const Loader = ({children, isLoading}) => {
+    if(!isLoading) return children
+    else return (<div className="spinner-border" id="spinner" role="status">
+    <span className="sr-only"></span>
+  </div>)
+}
+
+export default Loader
