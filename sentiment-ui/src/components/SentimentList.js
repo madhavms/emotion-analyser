b3f4b1f80336920cc58cd5810d077fd37d7883df
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-17 07:22:43 +0100

    Update SentimentList.js

--- sentiment-ui/src/components/SentimentList.js
+++ sentiment-ui/src/components/SentimentList.js
@@ -1,45 +1,110 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Paper,
+} from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Table = styled.table`
-  border-collapse: collapse;
-  width: 100%;
-  margin-bottom: 20px;
-`;
+const columns = [
+  { id: 'tweet', label: 'Tweet', minWidth: 300 },
+  { id: 'emotion', label: 'Emotion', minWidth: 150 },
+];
 
-const TableHeader = styled.th`
-  text-align: left;
-  background-color: #121212;
-  padding: 10px;
-`;
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#FFFFFF'
+    }
+  },
+  typography: {
+    allVariants: {
+      color: '#000000'
+    }
+  }
+});
 
-const TableData = styled.td`
-  border: 1px solid #ddd;
-  padding: 10px;
-`;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '90%',
+    margin: 'auto',
+  },
+  container: {
+    maxHeight: 500,
+  },
+  tableHeader: {
+    backgroundColor: '#FFFFFF',
+    color: '#000000',
+  }
+}));
+
 
 const SentimentList = ({ sentiments }) => {
-  console.log('sentiments=',typeof(sentiments))
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const classes = useStyles();
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
-    <div>
-      <h2>Sentiment Data</h2>
-      <Table>
-        <thead>
-          <tr>
-            <TableHeader>Tweet</TableHeader>
-            <TableHeader>Emotion</TableHeader>
-          </tr>
-        </thead>
-        <tbody>
-          {!!sentiments.length && sentiments.map((sentiment, index) => (
-            <tr key={index}>
-              <TableData>{sentiment.tweet}</TableData>
-              <TableData>{sentiment.Emotion}</TableData>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <Paper elevation={3}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader>
+              <TableHead className={classes.tableHeader}>
+                <TableRow className={classes.tableRow}>
+                  {columns.map((column) => (
+                    <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth, fontWeight: 'bold', fontSize: '0.95rem'}}>
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {sentiments.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((sentiment, index) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                      {columns.map((column) => {
+                        const value = sentiment[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.id === 'emotion' ? value.join(', ') : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={sentiments.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </div>
+    </ThemeProvider>
   );
 };
 
