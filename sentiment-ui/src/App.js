e972742b6b3dd7462e03a97ede654b4a36e87bc9
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-17 07:35:29 +0100

    Update App.js

--- sentiment-ui/src/App.js
+++ sentiment-ui/src/App.js
@@ -1,37 +1,61 @@
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import useSentiment from './hooks/useSentiment';
 import Loader from './components/Loader';
-import SentimentList from './components/SentimentList';
+import { TextField, Button, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+
 
 function App() {
-  const [keyword, setKeyword] = useState('');
+  const [keyword, setKeyword] = useState({});
+  const [loadingMessage, setLoadingMessage] = useState('');
   const [text, setText] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [tweets, sentiments] = useSentiment(keyword, setIsLoading);
+  const [displayComponent, setDisplayComponent] = useState('');
 
+  const [tweets, sentiments] = useSentiment(keyword, setIsLoading, setLoadingMessage);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(text)
-    if(text){
-      setKeyword(text);
+    if (text) {
+      setKeyword({ text });
       setIsLoading(true);
     }
-  }
+  };
+
+  const handleDisplayComponent = (component) => {
+    setDisplayComponent(component);
+  };
 
   return (
-    <div className="App">
-      <h1>Enter keyword for sentiment analysis</h1>
-      <form onSubmit={handleFormSubmit}>
-      <input placeholder="Enter keyword" onChange={e => setText(e.target.value)}/>
-      &nbsp;
-      <button type="submit" className='btn btn-primary' disabled={!text}>Submit</button>
-      </form>
-      <br></br>
-      <Loader isLoading={isLoading}>
-      {!!sentiments.length && <SentimentList sentiments={sentiments}/>}
-      </Loader>
+    <div>
+      <Navbar {...{title:'Emotion Analyser'}}/>
+      <Box sx={{ textAlign: 'center', mt: 16 }}>
+        <form onSubmit={handleFormSubmit}>
+          <TextField
+            label="Enter keyword"
+            variant="outlined"
+            size="small"
+            onChange={(e) => setText(e.target.value)}
+            sx={{ mr: 1, backgroundColor: 'white', color: 'white', borderColor: 'white' }}
+            autoComplete="off"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={!text}
+            sx={{ backgroundColor: text ? '#2196f3' : '#333333', color: 'white', '&:hover': { backgroundColor: text ? '#0d47a1' : '#333333' } }}
+          >
+            Submit
+          </Button>
+        </form>
+        <br />
+        <Loader isLoading={isLoading} loadingMessage={loadingMessage}>
+        <Dashboard {...{tweets, sentiments}}/>
+        </Loader>
+      </Box>
     </div>
   );
 }
